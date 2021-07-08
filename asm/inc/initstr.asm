da536e4fe9cfe86ab40ab3900626fe90aeeb8c82
Miro Haapalainen <32234658+Merplop@users.noreply.github.com>
2021-07-08 11:09:15 -0700

    Update initstr.asm

--- asm/inc/initstr.asm
+++ asm/inc/initstr.asm
@@ -1,3 +1,5 @@
+; strings printed on startup
+
 kinit.os_str: db 'MOSS (Merplop Open Source System) v0.04alpha', 0xA, 0xD, 0
 
 kinit.underscore_str: db '--------------------------------------------', 0xA, 0xD, 0
