c0c065b176950e334138b4d3ce0720081a82dc2b
Miro Haapalainen <32234658+Merplop@users.noreply.github.com>
2021-07-14 11:03:31 -0700

    Update bootsect.asm

--- asm/bootsect.asm
+++ asm/bootsect.asm
@@ -2,8 +2,7 @@
 ; Miro Haapalainen (Merplop), 2020-2021
 ; GNU GPL License v.3
 
-[BITS 16]         ; 16-bit real mode
-[ORG 0x7c00]	  	; load boot sector into memory offset 0xc700
+org 0x7c00	  	; load boot sector into memory offset 0xc700
 
 mov bx, 0x1000		; load into memory address 0x1000
 mov es, bx
