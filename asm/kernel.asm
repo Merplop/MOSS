2de3b58e463ece7dc430df06b62cfbdf31851a66
Miro Haapalainen <32234658+Merplop@users.noreply.github.com>
2023-02-01 11:52:33 -0800

    Restructure kernel source code, add hex print

--- asm/kernel.asm
+++ asm/kernel.asm
@@ -1,225 +1,223 @@
-; MOSS kernel
-; Miro Haapalainen (Merplop), 2020-2021
-; GNU GPL License v.3
+call mscreenr
 
-kinit:
-	call mscreenr
+mov si, welcome
+   call print_string
+ 
+ mainloop:
+   mov si, prompt
+   call print_string
+ 
+   mov di, buffer
+   call get_string
+ 
+   mov si, buffer
+   cmp byte [si], 0  ; blank line?
+   je mainloop       ; yes, ignore it
+ 
+   mov si, buffer
+   mov di, reboot_cmd
+   call strcmp
+   jc .reboot
+ 
+   mov si, buffer
+   mov di, help_cmd
+   call strcmp
+   jc .help
 
-	; print heading and info msg
-	mov si, .os_str
-	call print
+   mov si, buffer
+   mov di, ver_cmd
+   call strcmp
+   jc .ver
 
-	mov si, .underscore_str
-	call print
+   mov si, buffer
+   mov di, hex_cmd
+   call strcmp
+   jc .phex
+ 
+   mov si,badcommand
+   call print_string 
+   jmp mainloop  
+ 
+ .ver:
+   mov si, ver_str
+   call print_string
+ 
+   jmp mainloop
+ 
+ .help:
+   mov si, help_str
+   call print_string
+ 
+   jmp mainloop
 
-	mov si, .init_str
-	call print
+ .reboot:
+   jmp 0xFFFF:0x0000
 
-getinput:
-	mov si, prompt		; prints prompt to screen
-	call print		; calls print function
-	xor cx, cx		; clear cx register for looping through user input functions
-	mov si, cmdstr
+ .clear:
+   jmp clear_screen
 
-	mov ax, 0x2000
-	mov es, ax
-	mov ds, ax
-		
-keyloop:
-	xor ax, ax		; clear out accumulator
-	int 16h			; key stroke interrupt
+ .phex:
+   jmp print_hex
 
-	mov ah, 0x0e
-	cmp al, 0xD		; checks for "enter" key press
-	je execcmd
+ welcome: db 'MOSS (Merplop Open Source System) v0.04alpha', 0x0D, 0x0A, '--------------------------------------------', 0x0D, 0x0A, 'Type a command or run "help" for list of internal commands', 0x0D, 0x0A, 0
+ badcommand: db 'ERROR 01 - Invalid command', 0x0D, 0x0A, 0
+ prompt: db ':$', 0
+ skipline: db 0xA, 0xD, 0
 
-	int 0x10
-	mov [si], al
-	inc cx
-	inc si
-	jmp keyloop		; loop back to start of function
+ ; internal commands
 
-execcmd:
-	cmp cx, 0		; if loop count is zero, print cmd fail msg to screen
-	je cmdf
+ reboot_cmd:	db 'reb', 0		; reboot kernel
+ hcp_cmd:	    db 'hcp', 0		; halt cpu command
+ help_cmd:  	db 'help', 0		; print internal commands to screen
+ clr_cmd:	    db 'clr', 0		; clear screen
+ ver_cmd:	    db 'ver', 0		; display kernel version
+ ls_cmd:    	db 'ls', 0		; display contents of directory
+ colour_cmd:    db 'colour', 0		; display usage information for 'colour' command
+ hex_cmd:       db 'phex', 0
+ buffer: times 64 db 0
+ 
+ ; ================
+ ; calls start here
+ ; ================
+ 
+ print_string:
+   lodsb        ; grab a byte from SI
+ 
+   or al, al  ; logical or AL by itself
+   jz .done   ; if the result is zero, get out
+ 
+   mov ah, 0x0E
+   int 0x10      ; otherwise, print out the character!
+ 
+   jmp print_string
+ 
+ .done:
+   ret
+ 
+ get_string:
+   xor cl, cl
+ 
+ .loop:
+   mov ah, 0
+   int 0x16
+ 
+   cmp al, 0x08
+   je .backspace
+ 
+   cmp al, 0x0D
+   je .done
 
-	mov byte [si], 0 
-	mov si, cmdstr
+   cmp al, 0x0C
+   je clear_screen
+ 
+   cmp cl, 0x3F
+   je .loop
+ 
+   mov ah, 0x0E
+   int 0x10
+ 
+   stosb  ; put character in buffer
+   inc cl
+   jmp .loop
+   
+ .backspace:
+   cmp cl, 0	; beginning of string?
+   je .loop	; yes, ignore the key
+ 
+   dec di
+   mov byte [di], 0	; delete character
+   dec cl		; decrement counter as well
+ 
+   mov ah, 0x0E
+   mov al, 0x08
+   int 10h		; backspace on the screen
+ 
+   mov al, ' '
+   int 10h		; blank character out
+ 
+   mov al, 0x08
+   int 10h		; backspace again
+ 
+   jmp .loop	; go to the main loop
+ 
+ .done:
+   mov al, 0	; null terminator
+   stosb
+ 
+   mov ah, 0x0E
+   mov al, 0x0D
+   int 0x10
+   mov al, 0x0A
+   int 0x10		; newline
+ 
+   ret
+ 
+ strcmp:
+ .loop:
+   mov al, [si]   ; grab a byte from SI
+   mov bl, [di]   ; grab a byte from DI
+   cmp al, bl     ; are they equal?
+   jne .notequal  ; nope, we're done.
+ 
+   cmp al, 0  ; are both bytes (they were equal before) null?
+   je .done   ; yes, we're done.
+ 
+   inc di     ; increment DI
+   inc si     ; increment SI
+   jmp .loop  ; loop!
+ 
+ .notequal:
+   clc  ; not equal, clear the carry flag
+   ret
+ 
+ .done: 	
+   stc  ; equal, set the carry flag
+   ret
 
-chcmd:
-	push cx
-	mov di, reboot_cmd
-	repe cmpsb
-	je reboot
+clear_screen:
+   mov ah, 0x00	; set video mode
+   mov al, 0x03	; 80x25 text mode
+   int 0x10	; sets up interrupt handler for video services
 
-	pop cx
-	push cx
-	mov di, hcp_cmd
-	mov si, cmdstr
-	repe cmpsb
-	je halt_cpu
+   mov ah, 0x0B	; set colour scheme
+   mov bh, 0x00
+   mov bl, 0x09
+   int 0x10
 
-	pop cx
-	push cx
-	mov di, help_cmd
-	mov si, cmdstr
-	repe cmpsb
-	je help_msg
+   jmp mainloop
 
-	pop cx
-	push cx
-	mov di, ver_cmd
-	mov si, cmdstr
-	repe cmpsb
-	je ver_msg
+print_hex:
+   mov [.temp],al
+   shr al,4
+   cmp al,10
+   sbb al,69h
+   das
+ 
+   mov ah,0Eh
+   int 10h
+ 
+   mov al,[.temp]
+   ror al,4
+   shr al,4
+   cmp al,10
+   sbb al,69h
+   das
+ 
+   mov ah,0Eh
+   int 10h
+   mov si, skipline
+   call print_string
+ 
+   jmp mainloop
+ 
+   .temp db 0
+   
 
-	pop cx
-	push cx
-	mov di, clr_cmd
-	mov si, cmdstr
-	repe cmpsb
-	je clear_cmd
+; includescanvas
 
-	pop cx
-	push cx
-	mov di, colour0_cmd
-	mov si, cmdstr
-	repe cmpsb
-	je colour0
-
-	pop cx
-	push cx
-	mov di, colour1_cmd
-	mov si, cmdstr
-	repe cmpsb
-	je colour1
-
-	pop cx
-	push cx
-	mov di, colour2_cmd
-	mov si, cmdstr
-	repe cmpsb
-	je colour2
-
-	pop cx
-	push cx
-	mov di, colour3_cmd
-	mov si, cmdstr
-	repe cmpsb
-	je colour3
-
-	pop cx
-	push cx
-	mov di, colour4_cmd
-	mov si, cmdstr
-	repe cmpsb
-	je colour4
-
-	pop cx
-	push cx
-	mov di, colour5_cmd
-	mov si, cmdstr
-	repe cmpsb
-	je colour5
-
-	pop cx
-	push cx
-	mov di, colour6_cmd
-	mov si, cmdstr
-	repe cmpsb
-	je colour6
-
-	pop cx
-	push cx
-	mov di, colour7_cmd
-	mov si, cmdstr
-	repe cmpsb
-	je colour7
-
-	pop cx
-	push cx
-	mov di, colour8_cmd
-	mov si, cmdstr
-	repe cmpsb
-	je colour8
-
-	pop cx
-	push cx
-	mov di, colour9_cmd
-	mov si, cmdstr
-	repe cmpsb
-	je colour9
-
-	pop cx
-
-cmdf:
-	mov si, cmd_fail	; prints failure msg
-	call print
-	jmp getinput		; return to get input mode
-
-halt_cpu:
-	mov ah, 0x00
-	mov al, 0x03
-	int 0x10
-	popa
-	cli
-	hlt
-
-
-
-
-
-; internal commands
-
-reboot_cmd:	db 'reb', 0		; reboot kernel
-hcp_cmd:	db 'hcp', 0		; halt cpu command
-help_cmd:	db 'help', 0		; print internal commands to screen
-clr_cmd:	db 'clr', 0		; clear screen
-ver_cmd:	db 'ver', 0		; display kernel version
-
-; colour commands
-colour0_cmd:	db 'colour 0', 0		; change background colour to 0x00 
-colour1_cmd:	db 'colour 1', 0		; change background colour to 0x01
-colour2_cmd:	db 'colour 2', 0		; change background colour to 0x02
-colour3_cmd:	db 'colour 3', 0		; change background colour to 0x03
-colour4_cmd:	db 'colour 4', 0		; change background colour to 0x04
-colour5_cmd:	db 'colour 5', 0		; change background colour to 0x05
-colour6_cmd:	db 'colour 6', 0		; change background colour to 0x06
-colour7_cmd:	db 'colour 7', 0		; change background colour to 0x07
-colour8_cmd:	db 'colour 8', 0		; change background colour to 0x08
-colour9_cmd:	db 'colour 9', 0		; change background colour to 0x09
-
-; includes
-
-include "graph/mscreenr.asm"
-include "inc/help.asm"
-include "inc/ver.asm"
-include "graph/print.asm"
-include "inc/initstr.asm"
-include "inc/reboot.asm"
-include "graph/clr.asm"
-
-; colour command includes
-
-include "inc/colour/colour0.asm"
-include "inc/colour/colour1.asm"
-include "inc/colour/colour2.asm"
-include "inc/colour/colour3.asm"
-include "inc/colour/colour4.asm"
-include "inc/colour/colour5.asm"
-include "inc/colour/colour6.asm"
-include "inc/colour/colour7.asm"
-include "inc/colour/colour8.asm"
-include "inc/colour/colour9.asm"
-
-cmd_fail: db 0xA, 0xD, 'ERROR 01: Invalid command or filename', 0xA, 0xD, 0		; for byte definitions, 0xA is line feed, 0xD is carriage return, 0 is end
-
-prompt: db '$:', 0
-
-skipline: db 0xA, 0xD, 0
-
-colour_warning: db 'WARN - some BIOS colours may be difficult to read', 0xA, 0xD, 0
-
-cmdstr: db ''
-
-times 2048-($-$$) db 0
+ include "graph/mscreenr.asm"
+ include "inc/help.asm"
+ include "inc/ver.asm"
+ 
+   times 8192-($-$$) db 0
+   dw 0AA55h ; some BIOSes require this signature .clear_screen:
