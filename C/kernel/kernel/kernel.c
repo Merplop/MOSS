c6e703791c138989b0f7e452bf07ffbfe21584a4
Miro Haapalainen <32234658+Merplop@users.noreply.github.com>
2024-04-10 12:02:13 -0700

    Start process handler, add system and shell processes at startup

--- C/kernel/kernel/kernel.c
+++ C/kernel/kernel/kernel.c
@@ -4,40 +4,16 @@
 #include <kernel/tty.h>
 #include <kernel/keyboard.h>
 #include <kernel/kernel.h>
-//#include <>
-//#include <kernel/atoi.h>
 #include <sys/io.h>
-#define NUM_COMMANDS 16
-#define MAX_ARGS 10
-
-const char version[] = "Beta 1.0";
-const char welcome1_eng[] = "------------------------------\r\nMOSS Kernel - Version ";
-const char welcome1_fin[] = "------------------------------\r\nMOSS-kerneli - Versio ";
-const char welcome2[] = "\r\n------------------------------\r\n";
-const char prompt[] = ">";
-char cmd_str[256];
-char *cmd_str_ptr = cmd_str;
-uint8_t input_char = 0;
-uint8_t input_length;
-int argc = 0;
-char *argv[MAX_ARGS] = {0};
-int language = 0;
-const char newline[] = "\r\n";
-const char language_spacer_side[18] = {32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, '\0'};
-const char language_spacer_top[] = "\r\n\r\n\r\n\r\n";
-const char language_box_top[46] = {201,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,187,'\r', '\n', '\0'};
-const char language_box_title[] = "      MOSS Kernel - Choose Language      ";
-const char language_box_middle[46] = {204,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,185,'\r', '\n', '\0'}; 
-const char box_side[2] = {186, '\0'};
-const char language_box_bottom[46] = {200,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,188,'\r', '\n', '\0'};
-const char language_option1[] = "  1. English                             ";
-const char language_option2[] = "  2. suomi                               ";
-char* arg_token;
+#include <sys/sleep.h>
+//#define NUM_COMMANDS 17
+//#define MAX_ARGS 10
 
 Inode inodeList[1024];
 char fileNames[1024][32];
 mfs_file files[1024];
 mfs_dir dirs[1024];
+process processes[1024];
 size_t inodeCount = 1;
 uint32_t currentInode = 0;
 
@@ -45,10 +21,11 @@ int colour_scheme[] = {7, 0};
 int custom_colour_scheme = 0;
 
 
+
 int isdigit(char c) { return c >= '0' && c <= '9'; }
 
 int atoi(const char *str) {
-  uint8_t value = 0;
+  uint32_t value = 0;
   while (isdigit(*str)) {
     value *= 10;
     value += (*str) - '0';
@@ -57,6 +34,47 @@ int atoi(const char *str) {
 
   return value;
 }
+
+
+static void play_sound(uint32_t nFrequence) {
+	uint32_t Div;
+	uint8_t tmp;
+
+	Div = 1193180 / nFrequence;
+	outb(0x43, 0xb6);
+	outb(0x42, (uint8_t) (Div));
+	outb(0x42, (uint8_t) (Div >> 8));
+
+	tmp = inb(0x61);
+	if (tmp != (tmp | 3)) {
+		outb(0x61, tmp | 3);
+	}
+}
+
+void enable_speaker() {
+	uint8_t temp = inb(0x61);
+	outb(0x61, temp | 3);
+}
+
+void disable_speaker() {
+	uint8_t temp = inb(0x61);
+	outb(0x61, temp & 0xFC);
+}
+
+static void nosound() {
+	uint8_t tmp = inb(0x61) & 0xFC;
+	outb(0x61, tmp);
+}
+
+void beep_cmd() {
+	if (argc != 2) {
+		printf(ARG_COUNT_ERROR);
+		return;
+	}
+	int freq = atoi(argv[1]);
+	play_sound(freq);
+}
+
 
 int isFile(char* name) {
 	for (int i=0;i<1024;i++) {
@@ -146,24 +164,77 @@ void help_cmd(void) {
 		printf("sleep <s>        Sleep for s seconds\r\n");
 		printf("help             Display this\r\n");
 		printf("clear            Clear screen\r\n");
-		printf("exec <program>   Execute a program\r\n"); 
+		printf("ps				 View currently running processes\r\n"); 
 		printf("touch <name>     Create a file in working directory\r\n");
 		printf("mkdir <name>     Create a directory in working directory\r\n");
 		printf("cd <directory>   Change working directory\r\n");
 		printf("mv <file> <dir>  Move a file/directory into another directory\r\n");
 		printf("tex <filename>   Edit a MOSS-formatted data file\r\n");
+		printf("rm <filename>    Remove a file or directory\r\n");
+		printf("mv <filename>    Move a file or directory\r\n");
 	} else if (language == 1) {
-		const char help_msg_fin[] = {'S','i','s',132,'i','s','e','s','t','i',' ','m',132,132,'r','i','t','e','l','t','y',' ','k','o','m','e','n','n','o','t','\r','\n','\0'};
-		printf(help_msg_fin);
-                printf("ls               List contents of current directory\r\n");
-                printf("reb              Reboot computer\r\n");
-                printf("hlt              Halt CPU\r\n");
-                printf("shutdown         Shutdown computer\r\n");
-                printf("colour <fg> <bg> Change terminal colour\r\n");
-                printf("sleep <s>        Sleep for s seconds\r\n");
-                printf("help             Display this\r\n");
-                printf("clear            Clear screen\r\n");
-                printf("exec <program>   Execute a program\r\n");
+		printf("Sis"); 
+		putchar(132);
+		printf("isesti m"); putchar(132); putchar(132);
+		printf("ritelty komennot\r\n");
+                printf("ls               N"); putchar(132);
+		printf("yt");
+		putchar(132); 
+		printf(" nykyisen hakemiston sis");
+		putchar(132);
+		printf("ll");
+		putchar(148);
+		printf("n\r\n");
+                printf("reb              K");
+		putchar(132);
+		printf("ynnist");
+		putchar(132);
+		printf("tietokone uudelleen\r\n");
+                printf("hlt              Pys");
+		putchar(132);
+		printf("yt");
+	        putchar(132);
+	 	printf(" suoritin\r\n");
+                printf("shutdown         Sammuta tietokone\r\n");
+		printf("colour <fg> <bg> Vaihda p");
+		putchar(132);
+		putchar(132);
+		printf("tteen v");
+		putchar(132);
+		printf("ri");
+		putchar(132);
+		printf("\r\n");
+                printf("sleep <s>        Lep");
+		putchar(132);
+	        putchar(132);	
+		printf(" s sekunttia\r\n");
+                printf("help             N");
+		putchar(132);
+		printf("yt");
+		putchar(132);
+	        printf(" t");
+		putchar(132);
+		putchar('m');
+		putchar(132);
+		printf("\r\n");
+                printf("clear            Tyhjenn");
+		putchar(132);
+		printf(" p");
+		putchar(132);
+		putchar(132);
+		printf("te\r\n");
+		printf("exec <ohjelma>   Suorita ohjelma\r\n");
+		printf("touch <nimi>     Luo uusi tiedosto\r\n");
+		printf("mkdir <nimi>     Luo uusi hakemisto\r\n");
+		printf("cd <directory>   Vaihda hakemistoa\r\n");
+		printf("mv <tied> <hak>  Siirr");
+		putchar(132);
+		printf(" tiedosto/hakemisto toiseen hakemistoon\r\n");
+		printf("tex <nimi>       Muokkaa MOSSissa alustettua tiedostoa\r\n");
+		printf("rm <nimi>        Poista tiedosto/hakemisto\r\n");
+		printf("mv <nimi>        Siirr");
+		putchar(132);
+		printf(" tiedosto/hakemisto\r\n");
 	}
 }
 
@@ -180,21 +251,15 @@ void fetch_cmd(void) {
 		printf(ARG_COUNT_ERROR);
 		return;
 	}
-	printf("Kernel version %s\r\n", version);
-	printf("GNU General Public License v3.0\r\nC. Miro Haapalainen, 2024\r\n");
-	printf("MFS (MOSS Filesystem), working directory '%s'\r\n", fileNames[currentInode]);
-}
-
-void exec_cmd(void) {	
-}
-
-void asm_cmd(void) {
-	if (argc == 1) {
-		printf(ARG_COUNT_ERROR);
-		return;
+	if (language == 0) {
+		printf("Kernel version %s\r\n", version);
+		printf("GNU General Public License v3.0\r\nC. Miro Haapalainen, 2024\r\n");
+		printf("MFS (MOSS Filesystem), working directory '%s'\r\n", fileNames[currentInode]);
+	} else if (language == 1) {
+		printf("Kernelin versio %s\r\n", version);
+		printf("GNU GPL-julkinen lisenssi v3.0\r\nC. Miro Haapalainen, 2024\r\n");
+		printf("MFS (MOSS-tiedostojärjestelmä), nykyinen hakemisto '%s'\r\n", fileNames[currentInode]);
 	}
-//	const char arg[] = argv[1];
-//	__asm__ __volatile__(arg);
 }
 
 void ls_cmd(void) {
@@ -274,9 +339,7 @@ void touch_cmd(void) {
 		}
 		f1.data[k] = '\0';
 	} */
-	char test_contents[] = "This is a test, blah blah blah blah. This should be in a file.";
-	memcpy(&files[inodeCount].data, test_contents, strlen(test_contents));
-	files[inodeCount].size = strlen(test_contents);
+	files[inodeCount].size = 0;
 	memcpy(&files[inodeCount].type, "MDF", strlen("MDF"));
 	inodeList[inodeCount++] = i1;
 }
@@ -307,6 +370,19 @@ void mv_cmd(void) {
 
 }
 
+void rm_cmd(void) {
+	if (argc != 2) {
+		printf(ARG_COUNT_ERROR);
+		return;
+	}
+	int id = isFile(argv[1]);
+	if (id == -1) {
+		printf(FILE_NOT_FOUND_ERROR);
+		return;
+	}
+	
+}
+
 void cat_cmd(void) {
 	if (argc == 1) {
 		// TODO	
@@ -329,6 +405,70 @@ void cat_cmd(void) {
 	} else {
 		printf(ARG_COUNT_ERROR);
 		return;
+	}
+}
+
+void music_player(void) {
+	if (argc != 1) {
+		printf(ARG_COUNT_ERROR);
+		return;
+	}
+	int note_input;
+	int mult = 1;
+	change_colour_current(7,1);
+	terminal_initialize();
+	printf("PC Speaker Music Player\r\n");
+	printf("For list of notes, type ctrl+H\r\n");
+	while (1) {
+		note_input = get_key();
+		switch (note_input) {
+			case 'q':
+				play_sound(41*mult);
+				break;
+			case 'w':
+				play_sound(43*mult);
+				break;
+			case 'e':
+				play_sound(46*mult);
+				break;
+			case 'r':
+				play_sound(49*mult);
+				break;
+			case 't':
+				play_sound(52*mult);
+				break;
+			case 'y':
+				play_sound(55*mult);
+				break;
+			case 'u':
+				play_sound(58*mult);
+				break;
+			case 'i':
+				play_sound(61*mult);
+				break;
+			case 'o':
+				play_sound(65*mult);
+				break;
+			case 'p':
+				play_sound(69*mult);
+				break;
+		}
+		if (note_input == 'q') {
+			play_sound(82);
+		} else if (note_input == 'w') {
+			play_sound(87);
+		} else if (note_input == 'e') {
+			play_sound(92);
+		} else if (note_input == 'r') {
+			play_sound(98);
+		}
+	}
+}
+
+void ps_cmd() {
+	for (int i=0; i<process_count; i++) {
+		printf("%d   ", processes[i].pid);
+		printf("00:00:00   %s\r\n", processes[i].cmd);
 	}
 }
 
@@ -363,6 +503,7 @@ void text_editor(void) {
 				if (id == -1) {
 					argv[0] = "touch";
 					touch_cmd();
+					id = isFile(argv[1]);
 				}
 				for (int i=0;i<tex_input_length;i++) {
 					files[id].data[i] = tex_buffer[i];
@@ -396,8 +537,11 @@ void text_editor(void) {
 
 }
 
-char* commands[NUM_COMMANDS] = {"ls", "reb", "hlt", "shutdown", "colour", "sleep", "help", "clear", "fetch", "exec", "touch", "mkdir", "cd", "mv", "cat", "tex"};
-void (*command_ptrs[NUM_COMMANDS])(void) = {ls_cmd, reb_cmd, hlt_cmd, shutdown_cmd, colour_cmd, sleep_cmd, help_cmd, clear_cmd, fetch_cmd, exec_cmd, touch_cmd, mkdir_cmd, cd_cmd, mv_cmd, cat_cmd, text_editor};
+char* commands[NUM_COMMANDS] = {"sound", "stopsound", "tex", "cat", "ls", "reb", "hlt", "shutdown", 
+"colour", "sleep", "help", "clear", "fetch", "ps", "touch", "mkdir", "cd"};
+void (*command_ptrs[NUM_COMMANDS])(void) = {beep_cmd, nosound, text_editor, cat_cmd, ls_cmd, reb_cmd, hlt_cmd, shutdown_cmd, 
+colour_cmd, sleep_cmd, help_cmd, clear_cmd, fetch_cmd, ps_cmd, touch_cmd, mkdir_cmd, cd_cmd};
+
 
 void language_prompt(void) {
 	printf(language_spacer_top);
@@ -435,25 +579,13 @@ void language_prompt(void) {
 	}
 }
 
-void kernel_main(void) {
-	change_colour(7, 1);
-	memcpy(&fileNames[0], "root", strlen("root"));
-	dirs[0].size = 0;
-	terminal_initialize();
-	disable_cursor();
-	language_prompt();
-	change_colour(7, 0);
-	terminal_initialize();
+void start_shell() {
+	process shell;
+	shell.pid = 1;
+	shell.parent = &processes[0];
+	memcpy(&shell.cmd, "shell\0", strlen("shell\0"));
+	processes[process_count++] = shell;
 	enable_cursor();
-	if (language == 0) {
-		printf(welcome1_eng);
-		printf(version);
-		printf(welcome2); 
-	} else if (language == 1) {
-		printf(welcome1_fin);
-		printf(version);
-		printf(welcome2);
-	}
 	while(1) {
 		memset(cmd_str, 0, sizeof(cmd_str));
 		input_length = 0;
@@ -484,8 +616,6 @@ void kernel_main(void) {
 				}
 				continue;
 			}
-			// TODO - handle ASCII control characters,
-			// however the fuck you do that
 			if (input_char == 0x0C) {
 				clear_cmd();
 				break;
@@ -493,7 +623,10 @@ void kernel_main(void) {
 			cmd_str[input_length++] = input_char;
 			putchar(input_char);
 		}
-		// heyyyy ;)
+		char clear_ctrl_cmd[] = {0x0C};
+		if (memcmp(clear_ctrl_cmd, cmd_str, strlen(cmd_str)) == 0) {
+			continue;
+		}
 		arg_token = strtok(cmd_str, " ");
 		int command_found = 0;
 		int arg_error_caught = 0;
@@ -520,5 +653,31 @@ void kernel_main(void) {
 		}
 
 	}
+}
+
+void kernel_main(void) {
+	change_colour(7, 1);
+	memcpy(&fileNames[0], "root", strlen("root"));
+	dirs[0].size = 0;
+	process system;
+	system.pid = 0;
+	system.parent = &system;
+	memcpy(&system.cmd, "kernelsys\0", strlen("kernelsys\0"));
+	processes[process_count++] = system;
+	terminal_initialize();
+	disable_cursor();
+	language_prompt();
+	change_colour(7, 0);
+	terminal_initialize();
+	if (language == 0) {
+		printf(welcome1_eng);
+		printf(version);
+		printf(welcome2); 
+	} else if (language == 1) {
+		printf(welcome1_fin);
+		printf(version);
+		printf(welcome2);
+	}
+	start_shell();
 }
 
