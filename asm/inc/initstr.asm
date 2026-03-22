f48d8990721cd72d1125891baabb77402f7c2999
Miro Haapalainen <miro.haapalainen@gmail.com>
2026-03-22 12:38:33 -0700

    Refactoring

--- asm/inc/initstr.asm
+++
@@ -1,7 +0,0 @@
-; strings printed on startup
-
-kinit.os_str: db 'MOSS (Merplop Open Source System) v0.04alpha', 0xA, 0xD, 0
-
-kinit.underscore_str: db '--------------------------------------------', 0xA, 0xD, 0
-
-kinit.init_str: db 0xA, 0xD, 'Type a command or run "help" for list of internal commands', 0xA, 0xD, 0
--- asm/inc/reboot.asm
+++
@@ -1,4 +0,0 @@
-; reboot (reb) command
-
-reboot:
-	jmp 0xFFFF:0x0000	; jump to reset vector
